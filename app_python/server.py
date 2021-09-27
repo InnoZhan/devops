79af1de2eecf4df52161d0eb9dee275ad3d9daa5
InnoZhan <zhandos1609@gmail.com>
2021-09-27 23:42:08 +0300

    Lab 12

--- app_python/server.py
+++ app_python/server.py
@@ -21,11 +21,34 @@ def get_time():
     """
     timezone = pytz.timezone("Europe/Moscow")
     server_time = datetime.datetime.now(timezone)
+    print(str(server_time))
+    visits = []
+    with open("visits.txt", "r") as f: 
+        visits = f.readlines()
+    visits = visits[:100]
+    visits.insert(0, str(server_time)+'\n')
+    with open("visits.txt", "w") as f: 
+        f.writelines(visits)
     html_response = render_template(
         "index.html", time=str(server_time.strftime("%H:%M:%S"))
     )
     return html_response
 
+
+@app.route("/visits")
+def get_visits():
+    """Returns number of times root path was accessed
+
+    Returns:
+        int: number of times was accessed
+    """
+    visits = []
+    with open("visits.txt", "r") as f: 
+        visits = f.readlines()
+    html_response = render_template(
+        "visits.html", visits=visits
+    )
+    return html_response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
